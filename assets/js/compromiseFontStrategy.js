e00fb06158fd9e6cf89f11bff873d7d655e57692
Nanda Okitavera <codeharuka.yusa@gmail.com>
2018-12-19 21:29:08 +0700

    add babel-polyfill

--- assets/js/compromiseFontStrategy.js
+++ assets/js/compromiseFontStrategy.js
@@ -2,14 +2,14 @@
  * Critical FOFT with preload, with a polyfill fallback emulating font-display: optional
  * https://github.com/zachleat/web-font-loading-recipes
  */
-(function() {
+(function () {
   "use strict";
   // Optimization for Repeat Views
   if (sessionStorage.fontsLoadedCriticalFoftPreloadFallback) {
     document.documentElement.className += " webfont-stage-2";
     return;
   } else if ("fonts" in document) {
-    document.fonts.load("1em SourceSansProCriticalSubset").then(function() {
+    document.fonts.load("1em SourceSansProCriticalSubset").then(function () {
       document.documentElement.className += " webfont-stage-1";
       var latinFont = "SourceSansProLatinSubset";
       Promise.all([
@@ -19,7 +19,7 @@
         document.fonts.load("italic 1em " + latinFont),
         document.fonts.load("italic 700 1em " + latinFont),
         document.fonts.load("italic 300 1em " + latinFont)
-      ]).then(function() {
+      ]).then(function () {
         document.documentElement.className += " webfont-stage-2";
         // Optimization for Repeat Views
         sessionStorage.fontsLoadedCriticalFoftPreloadFallback = true;
@@ -30,6 +30,7 @@
     var ref = document.getElementsByTagName("script")[0];
     var script = document.createElement("script");
     script.src = "/assets/js/compromiseFallback.js";
+    script.type = "text/javascript";
     script.async = true;
     ref.parentNode.insertBefore(script, ref);
   }
