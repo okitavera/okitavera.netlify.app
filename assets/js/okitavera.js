6d776e413e3f20428f722c21a839c537df0b35ed
Nanda Okitavera <codeharuka.yusa@gmail.com>
2018-12-20 12:59:45 +0700

    button-driven disqus loading

--- assets/js/okitavera.js
+++ assets/js/okitavera.js
@@ -1,14 +1,10 @@
-/*!
- * Copyright (c) 2018 Nanda Okitavera
- * MIT License
- * https://github.com/okitavera/okitavera.me
- */
-
 import LazyLoad from "vanilla-lazyload";
 import SmoothScroll from "smooth-scroll";
+
 new LazyLoad({
   elements_selector: ".---ll"
 });
+
 new SmoothScroll('a[href*="#"]', {
   topOnEmptyHash: true,
   easing: "easeOutQuint",
@@ -19,45 +15,70 @@ new SmoothScroll('a[href*="#"]', {
   speedAsDuration: true
 });
 
-(() => {
-  const visible = (el, state) => {
-    el.style.visibility = state === 1 ? "visible" : "hidden";
-    el.style.opacity = state;
+const visible = (el, state) => {
+  el.style.visibility = state === 1 ? "visible" : "hidden";
+  el.style.opacity = state;
+};
+const overlaylink = document.querySelectorAll("#ovm--btn,.mobile-menu a");
+const overlayMenu = document.querySelector(".mobile-menu");
+visible(overlayMenu, 0);
+overlaylink.forEach((el) => {
+  el.onclick = () => {
+    const overlayButton = document.querySelector("#ovm--btn");
+    const state = overlayMenu.style.visibility === "hidden" ? 1 : 0;
+    visible(overlayMenu, state);
+    state
+      ? overlayButton.classList.add("opened")
+      : overlayButton.classList.remove("opened");
   };
-  const overlaylink = document.querySelectorAll("#ovm--btn,.mobile-menu a");
-  const overlayMenu = document.querySelector(".mobile-menu");
-  visible(overlayMenu, 0);
-  overlaylink.forEach((el) => {
-    el.onclick = () => {
-      const overlayButton = document.querySelector("#ovm--btn");
-      const state = overlayMenu.style.visibility === "hidden" ? 1 : 0;
-      visible(overlayMenu, state);
-      state
-        ? overlayButton.classList.add("opened")
-        : overlayButton.classList.remove("opened");
-    };
-  });
+});
 
-  const body = document.documentElement || document.body;
-  var scrolling = false;
-  window.onscroll = () => (scrolling = true);
-  setInterval(() => {
-    if (scrolling) {
-      scrolling = false;
-      visible(
-        document.querySelector(".backtotop"),
-        body.scrollTop > body.clientHeight / 4 ? 1 : 0
-      );
-    }
-  }, 250);
+const body = document.documentElement || document.body;
+var scrolling = false;
+window.onscroll = () => (scrolling = true);
+setInterval(() => {
+  if (scrolling) {
+    scrolling = false;
+    visible(
+      document.querySelector(".backtotop"),
+      body.scrollTop > body.clientHeight / 4 ? 1 : 0
+    );
+  }
+}, 250);
 
-  if (document.querySelector("#disqus_thread") !== null) {
+const dqFrame = "disqus_thread";
+
+function loadDisqus() {
+  if (!window.disqusFrame) {
+    document.getElementById("btnDQ").style.display = "none";
+    window.disqusFrame = true;
+    const username = document.getElementById(dqFrame).getAttribute("data-disqus-username");
     const script = document.createElement("script");
-    const username = document
-      .querySelector("#disqus_thread")
-      .getAttribute("data-disqus-username");
+    script.type = "text/javascript";
     script.src = `https://${username}.disqus.com/embed.js`;
+    script.async = true;
     script.setAttribute("data-timestamp", +new Date());
     (document.head || document.body).appendChild(script);
   }
-})();
+}
+
+if (/bot|google|baidu|bing|msn|duckduckgo|slurp|yandex/i.test(navigator.userAgent)) {
+  loadDisqus();
+} else if (document.getElementById(dqFrame)) {
+  window.disqusFrame = false;
+  if (!document.getElementById("btnDQ")) {
+    const btn = document.createElement("button");
+    btn.className = "button button-outline"
+    btn.id = "btnDQ";
+    btn.innerText = "view comments";
+    btn.onclick = () => loadDisqus();
+    document.getElementById(dqFrame).appendChild(btn);
+    document.getElementById(dqFrame).align = "center";
+  }
+}
+
+/*!
+ * Copyright (c) 2018 Nanda Okitavera
+ * MIT License
+ * https://github.com/okitavera/okitavera.me
+ */
