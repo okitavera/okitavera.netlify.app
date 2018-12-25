0e61cb326b2c5be8e3da7a3c782973a4d3306bc9
okitavera <codeharuka.yusa@gmail.com>
2018-12-26 01:11:54 +0700

    back to lato

--- assets/js/FontLoaderData.js
+++ assets/js/FontLoaderData.js
@@ -1,10 +1,19 @@
 module.exports = {
   critical: {
-    name: "SourceSansProCriticalSubset",
+    name: "LatoCriticalSubset",
     variant: ["400"]
   },
   final: {
-    name: "SourceSansProLatinSubset",
-    variant: ["400", "300", "700", "400 italic", "300 italic", "700 italic"]
+    name: "LatoLatinSubset",
+    variant: [
+      "400",
+      "300",
+      "700",
+      "900",
+      "400 italic",
+      "300 italic",
+      "700 italic",
+      "900 italic"
+    ]
   }
 };
