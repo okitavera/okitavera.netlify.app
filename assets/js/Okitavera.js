21290d052a5afa7ea3937984f44fc7de5e4b4b84
okitavera <codeharuka.yusa@gmail.com>
2018-12-26 19:17:51 +0700

    delete old js code

--- assets/js/Okitavera.js
+++ assets/js/Okitavera.js
@@ -10,22 +10,11 @@ new LazyLoad({
 document.documentElement.style.scrollBehavior = "smooth";
 
 const visible = (el, state) => {
-  el.style.visibility = state === 1 ? "visible" : "hidden";
-  el.style.opacity = state;
+  if (el) {
+    el.style.visibility = state === 1 ? "visible" : "hidden";
+    el.style.opacity = state;
+  }
 };
-const overlaylink = document.querySelectorAll("#ovm--btn,.mobile-menu a");
-const overlayMenu = document.querySelector(".mobile-menu");
-visible(overlayMenu, 0);
-overlaylink.forEach((el) => {
-  el.onclick = () => {
-    const overlayButton = document.querySelector("#ovm--btn");
-    const state = overlayMenu.style.visibility === "hidden" ? 1 : 0;
-    visible(overlayMenu, state);
-    state
-      ? overlayButton.classList.add("opened")
-      : overlayButton.classList.remove("opened");
-  };
-});
 
 const body = document.documentElement || document.body;
 var scrolling = false;
