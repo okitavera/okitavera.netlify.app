120e40ee1970156794d61f571801fe743979e032
okitavera <codeharuka.yusa@gmail.com>
2019-01-03 21:42:14 +0700

    move webmanifest to the root folder

--- .eleventy.js
+++ .eleventy.js
@@ -15,6 +15,7 @@ module.exports = (eleventy) => {
   // so we only copy imgs and fonts
   eleventy.addPassthroughCopy("assets/img");
   eleventy.addPassthroughCopy("assets/fonts");
+  eleventy.addPassthroughCopy("webmanifest.json");
   eleventy.addPassthroughCopy("robots.txt");
   eleventy.addPlugin(pluginRss);
   eleventy.addPlugin(pluginSyntaxHighlight);
