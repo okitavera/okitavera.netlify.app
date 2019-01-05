9511cd1025c3398302e605e05446d35d1d78e10e
okitavera <codeharuka.yusa@gmail.com>
2019-01-05 15:30:27 +0700

    caching webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const metadata = require("./data/manifest/metadata.json");
 const TerserPlugin = require("terser-webpack-plugin");
+const HardSourceWebpackPlugin = require("hard-source-webpack-plugin");
 
 const config = {
   mode: "production",
@@ -11,20 +12,26 @@ const config = {
         test: /\.m?js$/,
         include: path.resolve(__dirname, "assets"),
         use: {
-          loader: "babel-loader"
+          loader: "babel-loader?cacheDirectory"
         }
       }
     ]
   },
   plugins: [
+    new HardSourceWebpackPlugin(),
     new webpack.DefinePlugin({
       disqusdata: JSON.stringify(metadata.disqus)
     })
   ],
+  output: {
+    pathinfo: false
+  },
   optimization: {
     minimizer: [
       new TerserPlugin({
+        cache: true,
         parallel: true,
+        sourceMap: false,
         terserOptions: {
           output: {
             comments: false
