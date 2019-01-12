dcf42fcca27d9f1a2d343bc2eb41b14d8eed7393
okitavera <codeharuka.yusa@gmail.com>
2019-01-12 17:13:15 +0700

    add mjs to the list of cached files

--- .eleventy.js
+++ .eleventy.js
@@ -21,6 +21,7 @@ module.exports = (eleventy) => {
   eleventy.addPlugin(pluginRss);
   eleventy.addPlugin(pluginSyntaxHighlight);
   eleventy.addPlugin(pluginPWA, {
+    globPatterns: ["**/*.{html,css,js,mjs,jpg,png,gif,webp,svg,woff2,woff}"],
     clientsClaim: true,
     skipWaiting: true
   });
