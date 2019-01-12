8d046de3d63e4fba4252e1a02d9f22acbab762de
okitavera <codeharuka.yusa@gmail.com>
2019-01-12 07:57:53 +0700

    fix popstate behaviour

--- assets/js/modules/PageLoader.js
+++ assets/js/modules/PageLoader.js
@@ -1,6 +1,9 @@
 import isExternal from "is-url-external";
 
 const PageLoader = (loaderDone) => {
+  const STATE_POP = 10,
+    STATE_PUSH = 11;
+
   function linksListener() {
     var links = document.querySelectorAll("a:not([href^='#'])");
     links.forEach((a) => {
@@ -8,24 +11,24 @@ const PageLoader = (loaderDone) => {
       if (!isExternal(url) && !url.match(/\/\#/g)) {
         a.onclick = (e) => {
           e.preventDefault();
-          getPage(url);
+          getPage(url, STATE_PUSH);
         };
       }
     });
   }
 
-  function getPage(url) {
+  function getPage(url, type = STATE_POP) {
     var parser = new DOMParser();
     fetch(url)
       .then((response) => response.text().then((text) => Promise.resolve(text)))
       .then((html) =>
-        renderPage(parser.parseFromString(html, "text/html"), url)
+        renderPage(parser.parseFromString(html, "text/html"), url, type)
       )
       .then(linksListener)
       .then(loaderDone);
   }
 
-  function renderPage(page, url = null) {
+  function renderPage(page, url = null, type = STATE_POP) {
     if (page) {
       document.title = page.title;
       document.querySelectorAll("title,main").forEach((el, i) => {
@@ -41,19 +44,15 @@ const PageLoader = (loaderDone) => {
         }
       });
     }
-    if (url != null) {
-      var state = {
-        url: url,
-        contents: JSON.stringify(page)
-      };
-      history.pushState(state, page.title, url);
-      document.documentElement.style.scrollBehavior = "unset";
-      document.body.scrollTop = document.documentElement.scrollTop = 0;
+    if (url != null && type != STATE_POP) {
+      history.pushState({ location: url }, page.title, url);
     }
+    document.documentElement.style.scrollBehavior = "unset";
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
   }
 
-  window.addEventListener("popstate", function(event) {
-    renderPage(event.state);
+  window.addEventListener("popstate", function(ev) {
+    if (ev.state != null) getPage(ev.state.location, STATE_POP);
   });
   linksListener();
 };
