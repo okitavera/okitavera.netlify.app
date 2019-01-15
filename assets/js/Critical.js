ac8f926ac86e335c11309013090019fb54b241c3
okitavera <codeharuka.yusa@gmail.com>
2019-01-15 12:18:07 +0700

    simplify js detection

--- assets/js/Critical.js
+++ assets/js/Critical.js
@@ -2,11 +2,7 @@ import fontLoader from "./modules/FontLoader";
 import fontData from "./FontLoaderData";
 
 fontLoader(fontData);
-
-document.documentElement.className = document.documentElement.className.replace(
-  "no-js",
-  "has-js"
-);
+document.documentElement.classList.remove("no-js");
 
 if ("serviceWorker" in navigator)
   navigator.serviceWorker.register(`/service-worker.js?v=${buildstamp}`);
