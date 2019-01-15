cbbdb8d644314aa8ff2583328edd050c061a6ff6
okitavera <codeharuka.yusa@gmail.com>
2019-01-15 10:55:24 +0700

    remove unneeded css inject code

--- assets/js/Critical.js
+++ assets/js/Critical.js
@@ -10,10 +10,3 @@ document.documentElement.className = document.documentElement.className.replace(
 
 if ("serviceWorker" in navigator)
   navigator.serviceWorker.register(`/service-worker.js?v=${buildstamp}`);
-
-var loadIcons = document.getElementsByTagName("link")[0];
-var cssIcons = document.createElement("link");
-cssIcons.rel = "stylesheet";
-cssIcons.href = "/assets/css/IlluminateIcons.css";
-cssIcons.type = "text/css";
-loadIcons.parentNode.insertBefore(cssIcons, loadIcons);
