771d07a0b502808326e3653bc08b32ba2276341c
okitavera <codeharuka.yusa@gmail.com>
2019-01-15 14:41:07 +0700

    back to the basic

--- assets/js/Critical.js
+++ assets/js/Critical.js
@@ -1,7 +1,1 @@
-// import fontLoader from "./modules/FontLoader";
-// import fontData from "./FontLoaderData";
-// fontLoader(fontData);
-
 document.documentElement.classList.remove("no-js");
-if ("serviceWorker" in navigator)
-  navigator.serviceWorker.register(`/service-worker.js?v=${buildstamp}`);
--- eleventy.d/plugins.js
+++ eleventy.d/plugins.js
@@ -1,15 +1,7 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginPWA = require("eleventy-plugin-pwa");
 
 module.exports = ({ eleventy }) => {
   eleventy.addPlugin(pluginRss);
   eleventy.addPlugin(pluginSyntaxHighlight);
-  eleventy.addPlugin(pluginPWA, {
-    globPatterns: [
-      "**/*.{html,css,js,mjs,jpg,png,gif,webp,svg,woff2,woff,json,ico}"
-    ],
-    clientsClaim: true,
-    skipWaiting: true
-  });
 };
