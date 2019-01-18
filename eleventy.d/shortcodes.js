5b77061a8b69a390ed8e05174fb3e56c5f035929
okitavera <codeharuka.yusa@gmail.com>
2019-01-18 22:47:55 +0700

    add icon posts for eyecandy, background generated by js

--- eleventy.d/shortcodes.js
+++ eleventy.d/shortcodes.js
@@ -10,4 +10,12 @@ module.exports = ({ eleventy }) => {
     const yrNow = new Date().getFullYear();
     return firstYear.concat("-" + yrNow);
   });
+
+  eleventy.addShortcode("generateColorFrom", (title) => {
+    var hash = title
+      .split("")
+      .reduce((prev, curr) => ((prev << 5) - prev + curr.charCodeAt(0)) | 0, 0);
+    var hue = hash % 360;
+    return `hsl(${hue}, 36%, 63%)`;
+  });
 };
