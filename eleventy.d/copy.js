56f64d19ee6c2b589178594e3c582d75ab794606
okitavera <codeharuka.yusa@gmail.com>
2019-02-19 12:49:27 +0700

    initial netlifycms implementation

--- eleventy.d/copy.js
+++ eleventy.d/copy.js
@@ -3,4 +3,5 @@ module.exports = ({ eleventy }) => {
   eleventy.addPassthroughCopy("assets/fonts");
   eleventy.addPassthroughCopy("webmanifest.json");
   eleventy.addPassthroughCopy("robots.txt");
+  eleventy.addPassthroughCopy("admin");
 };
