a06c7e87d0605a5a25d3f12ac7f09ca55ee944c0
Nanda Oktavera <codeharuka.yusa@gmail.com>
2020-03-03 19:39:14 +0900

    serve my app-ads.txt
    Signed-off-by: Nanda Oktavera <codeharuka.yusa@gmail.com>

--- eleventy.d/copy.js
+++ eleventy.d/copy.js
@@ -3,5 +3,6 @@ module.exports = ({ eleventy }) => {
   eleventy.addPassthroughCopy("assets/fonts");
   eleventy.addPassthroughCopy("webmanifest.json");
   eleventy.addPassthroughCopy("robots.txt");
+  eleventy.addPassthroughCopy("app-ads.txt");
   eleventy.addPassthroughCopy("admin");
 };
